d7a3673abe3c0f09157cad08d7697db9539130af
Onahi7 <dicksonhardy7@gmail.com>
2025-12-18 14:21:45 +0100

    i have fixed

--- app/studio/[[...index]]/layout.tsx
+++ app/studio/[[...index]]/layout.tsx
@@ -1,18 +1,7 @@
-import { redirect } from 'next/navigation'
-import { auth } from '@/lib/auth'
-
-export const dynamic = 'force-dynamic'
-
-export default async function StudioLayout({
+export default function StudioLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const session = await auth()
-
-  if (!session?.user) {
-    redirect('/admin/login?callbackUrl=/studio')
-  }
-
   return children
 }
